343a820ebdfb95174648dc3d5ee069d3404eb8a4
Pablo Eduardo <pabloeduraimundo@gmail.com>
2025-10-09 22:05:42 -0300

    Cadastro do aluno

---
+++ php/filtro.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+    <link rel="stylesheet" href="css/front.css" />
+    <link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon" />
+    <link
+        rel="stylesheet"
+        type="text/css"
+        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" />
+
+    <title>Pesquisa</title>
+</head>
+
+<body>
+    <header>
+        <nav class="navegacao">
+            <a href="index.html" class="logo"><i class="fa-solid fa-house"></i> NAE </a>
+            <div class="links">
+                <a href="../html/cadastro_aluno.html">Cadastro</a>
+                <a href="filtro.php">Pesquisa</a>
+            </div>
+        </nav>
+    </header>
+    <main>
+        <div class="container">
+
+        </div>
+    </main>
+    <footer>
+        <div class="rodape">
+            <h3>Feito por Pablo 2025</h3>
+        </div>
+    </footer>
+</body>
+
+</html>
